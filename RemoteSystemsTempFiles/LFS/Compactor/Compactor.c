d496a9d1c392dedb99089fa7e40ae6357a9b35d8
JaniceFassi <janicebahiana@gmail.com>
2019-04-08 16:26:05 -0300

    Mas estructura

---
+++ RemoteSystemsTempFiles/LFS/Compactor/Compactor.c
@@ -0,0 +1,13 @@
+/*
+ * Compactor.c
+ *
+ *  Created on: 8 abr. 2019
+ *      Author: utnso
+ */
+
+int main(){
+
+
+
+	return 0;
+}
---
+++ RemoteSystemsTempFiles/LFS/Compactor/Compactor.h
@@ -0,0 +1,13 @@
+/*
+ * Compactor.h
+ *
+ *  Created on: 8 abr. 2019
+ *      Author: utnso
+ */
+
+#ifndef COMPACTOR_H_
+#define COMPACTOR_H_
+
+
+
+#endif /* COMPACTOR_H_ */
