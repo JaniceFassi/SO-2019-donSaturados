3015da5ed4033cd88b7f3af8f7bd233c5d459fbe
Mora Pelozzi <pelozzimora@gmail.com>
2019-04-19 21:11:30 -0300

    Agrego la estructura de sockets al mempool sin usar la biblioteca que armamos

--- MemPool/src/memPrincipal.c
+++ MemPool/src/memPrincipal.c
@@ -11,11 +11,76 @@
 
 #include "memPrincipal.h"
 
-
-
 int main(void) {
 
-	int protocoloFuncion;
+
+	//SERVIDOR PARA KERNEL
+	struct sockaddr_in servidorParaKernel;
+	servidorParaKernel.sin_family = AF_INET;
+	servidorParaKernel.sin_addr.s_addr = inet_addr("192.168.1.116");
+	servidorParaKernel.sin_port = htons(7700);
+
+	int servidorKernel= socket(AF_INET,SOCK_STREAM,0);
+
+	if(servidorKernel==-1){
+		perror("Error al crear socket\n");
+		return 1;
+		}
+
+	int activado=1;
+
+	if(setsockopt(servidorKernel, SOL_SOCKET, SO_REUSEADDR, (void*)&activado, (socklen_t)sizeof(activado))==-1){
+		perror("Error al setear el socket\n");
+		return 1;
+		}
+
+
+	if(bind(servidorKernel, (void*) &servidorParaKernel, sizeof(servidorParaKernel))!=0){
+		perror("Fallo el bind\n");
+		return 1;
+			}
+
+
+	listen(servidorKernel,100);
+	printf("Servidor escuchando\n");
+
+	struct sockaddr_in direccionCliente;
+	unsigned int tamanioDireccion=sizeof(direccionCliente);
+
+	int clienteKernel = accept(servidorKernel, (void*) &direccionCliente, (socklen_t*)&tamanioDireccion);
+
+
+	if(clienteKernel<0){
+		perror("Error al aceptar la conexion");
+			}
+
+	printf("Se conecto un cliente\n");
+
+
+	//CLIENTE PARA LISSANDRA
+
+	struct sockaddr_in servidorLissandra;
+
+	servidorLissandra.sin_family = AF_INET;
+	servidorLissandra.sin_addr.s_addr = inet_addr("192.168.1.116");
+	servidorLissandra.sin_port = htons(7700);
+
+	int lissandraServer = socket(AF_INET, SOCK_STREAM, 0);
+
+	if(lissandraServer < 0){
+		perror("No se pudo crear el socket cliente");
+	}
+
+	if(connect(lissandraServer, (struct sockaddr *) &servidorLissandra, sizeof(servidorLissandra)) !=0){
+		perror("No se pudo conectar al servidor Lissandra");
+		return 1;
+		}
+
+
+
+
+
+	int protocoloFuncion = 0;
 
 	switch(protocoloFuncion){
 		case 0:
@@ -39,7 +104,6 @@ int main(void) {
 
 
 	}
-
 
 
 	return EXIT_SUCCESS;
