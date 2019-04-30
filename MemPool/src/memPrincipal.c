126995646c8f6fc255a3c04995b291dca3b7dfd9
Franco Cavallini <43447255+francocava@users.noreply.github.com>
2019-04-30 19:50:29 -0300

    Add files via upload
    para leer el config y hacer el log

--- MemPool/src/memPrincipal.c
+++ MemPool/src/memPrincipal.c
@@ -8,7 +8,6 @@
  ============================================================================
  */
 
-
 #include "memPrincipal.h"
 
 Lista *primero = NULL;
@@ -17,21 +16,29 @@ Lista *ultimo = NULL;
 int main(void) {
 
 	// CONEXIONES
-	//KERNEL
+	// KERNEL
 
+	t_log* logger = init_logger();
+    t_config* config = read_config();
+
+	char* ipLFS = config_get_string_value(config,"IP_FS");
+	char* puertoLFS = config_get_string_value(config,"PUERTO_FS");
+
+
+	// Conexion kernel
 	u_int16_t kernelServer;
-	char * ipKernel = "127.0.0.1";
-	u_int16_t puertoKernel= 7000;
+	char * ip = "127.0.0.1";
+	u_int16_t port= 7000;
 
-	if(createServer(ipKernel,puertoKernel,&kernelServer)!=0){
+	if(createServer(ip,port,&kernelServer)!=0){
 		printf("se me rompio el programa");
 		return 1;
 	}
 
 
-
-	listenForClients(kernelServer,100);
+	listenForClients(kernelServer,100); // esta funcion esta al pedo
 	printf("\nEstoy escuchando\n");
+
 
 	u_int16_t kernelClient;
 	char * p="me llamo Kernel wacho";
@@ -41,7 +48,6 @@ int main(void) {
 		return 1;
 	}
 	printf("lo logramos!!");
-
 
 	//LISSANDRA
 	u_int16_t lfsCliente;
@@ -59,50 +65,52 @@ int main(void) {
 
 /////////////////////////////
 
-
 	int protocoloFuncion = 0;
 
 	switch(protocoloFuncion){
 		case 0:
-			mSelect();
+			//mSelect(char* tableName,u_int16_t key);
 			break;
 		case 1:
 			char* nombreTabla;
 			scanf(String, &nombreTabla);
 			u_int16_t keyTabla;
 			char* value;
-
-			mInsert(nombreTabla, keyTabla, value);
+			mInsert(nombreTabla, keyTabla, valor);
 			break;
 
+			break;
 		case 2:
 			mCreate();
 			break;
-
 		case 3:
 			mDescribe();
 			break;
-
 		case 4:
 			mDrop();
 			break;
-
 		case 5:
 			mJournal();
 			break;
-
-
 	}
-
 
 	return EXIT_SUCCESS;
 }
 
-void mSelect(){
+
+t_config* read_config() {
+	return config_create("/home/utnso/tp-2019-1c-donSaturados/MemPool/Mem.config");
+}
+
+t_log* init_logger() {
+	return log_create("memPrincipal.log", "memPrincipal", 1, LOG_LEVEL_INFO);
+}
+
+
+void mSelect(char* nombreTabla,u_int16_t keyTabla){
 
 }
-void mInsert(char* nombreTabla, u_int16_t keyTabla, char* valor){
-
+void mInsert(char* nombreTabla,u_int16_t keyTabla,char* valor){
 
 }
 void mCreate(){
