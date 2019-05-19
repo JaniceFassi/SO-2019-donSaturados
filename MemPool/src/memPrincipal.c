5b08dbd94926f149b9c1eef8d89bcb4db5c247bb
Franco Cavallini <43447255+francocava@users.noreply.github.com>
2019-05-19 17:19:26 -0300

    actualizo la mem en master

--- MemPool/src/memPrincipal.c
+++ MemPool/src/memPrincipal.c
@@ -10,56 +10,35 @@
 
 #include "memPrincipal.h"
 
+t_list* segmentoLista;
+t_log* logger;
+t_config *config;
+u_int16_t tamanioMaximo;
 
 int main(void) {
 
-	// CONEXIONES
-	// KERNEL
 
-	t_log* logger = init_logger();
-    t_config* config = read_config();
-    t_list* segmentoLista = list_create(); //no se bien como se maneja las commons para los elementos de esta lista
-
-
-	// Conexion kernel
-	u_int16_t kernelServer;
-	char * ip = "127.0.0.1";
-	u_int16_t port= 7000;
-
-	if(createServer(ip,port,&kernelServer)!=0){
-		printf("se me rompio el programa");
-		return 1;
-	}
-
-
-	listenForClients(kernelServer,100); // esta funcion esta al pedo
-	printf("\nEstoy escuchando\n");
-
-
-	u_int16_t kernelClient;
-	char * p="me llamo Kernel wacho";
-
-	if(acceptConexion(kernelServer,&kernelClient,p,0,0)!=0){
-		printf("habiamos llegado tan lejos...");
-		return 1;
-	}
-	printf("lo logramos!!");
+	//INICIALIZO
+	logger = init_logger();
+	config = read_config();
+	segmentoLista = list_create(); //Va a ser nuestra solucion pedorra de memoria por el momento, un unico segmento con  una sola pagina.
 
 	//LISSANDRA
-	//la idea aca es hacer el handshake con LFS y recibir el value maximo obvio que no esta terminado
+	//la idea aca es hacer el handshake con LFS y recibir el value maximo, obvio que no esta terminado
 
 	u_int16_t lfsCliente;
 	char * ipLissandra = config_get_string_value(config,"IP_FS");
 	u_int16_t puertoLissandra = config_get_string_value(config,"PUERTO_FS");
 
-	int conexionExitosa;
+	handshakeConLissandra(&lfsCliente,ipLissandra,puertoLissandra);
+	//Lo comento por ahora para que no me tire los errores
 
-	conexionExitosa = linkClient(&lfsCliente,ipLissandra , puertoLissandra);
+	mInsert("TABLA1",57,"DUKI");
+	mInsert("TABLA1",57,"DUKI2");
+	mInsert("TABLA2",59,"Esto funciona");
+	mInsert("TABLA2",566,"Esto funciona2");
 
-	if(conexionExitosa !=0){
-		printf("Error al conectarse con LFS"); // no es mejor el perror aca?
-	}
-
+	verMemoria();
 
 
 /////////////////////////////
@@ -71,12 +50,7 @@ int main(void) {
 			//mSelect(char* nombreTabla,u_int16_t keyTabla);
 			break;
 		case 1:
-			// esto me rompia asi que lo comente
-			//char* nombreTabla;
-			//scanf(String, &nombreTabla);
-			//u_int16_t keyTabla;
-			//char* valor;
-			//mInsert(nombreTabla, keyTabla, valor);
+			//mInsert()
 			break;
 
 			break;
@@ -97,29 +71,53 @@ int main(void) {
 	return EXIT_SUCCESS;
 }
 
+//Le robe la consola a SoliJani total es la misma logica
+void consola(){
 
-t_config* read_config() {
-	return config_create("/home/utnso/tp-2019-1c-donSaturados/MemPool/Mem.config");
+	char* linea;
+	while(1){
+		//linea = readline(">"); Por que no me reconoce readLine??
+
+		if(!strncmp(linea,"SELECT ",7))
+			{
+				//selectS();
+			}
+		if(!strncmp(linea,"INSERT ",7)){
+		 		//insert(linea);
+		 	}
+		if(!strncmp(linea,"CREATE ",7)){
+				//create();
+			}
+		if(!strncmp(linea,"DESCRIBE ",9)){
+				//describe();
+			}
+		if(!strncmp(linea,"DROP ",5)){
+				//	drop();
+			}
+
+		if(!strncmp(linea,"exit",5)){
+				free(linea);
+			}
+			free(linea);
+		}
 }
-
-t_log* init_logger() {
-	return log_create("memPrincipal.log", "memPrincipal", 1, LOG_LEVEL_INFO);
-}
-
-Segmento *crearSegmento(char* nombre,u_int16_t key){
-	Segmento *nuevo = malloc(sizeof(Segmento));
-	nuevo->nombre = nombre;
-	nuevo->keyTabla = key;
-	return nuevo;
-}
-
 
 void mSelect(char* nombreTabla,u_int16_t keyTabla){
 
 }
 void mInsert(char* nombreTabla,u_int16_t keyTabla,char* valor){
 
+	if(buscarYreemplazar(keyTabla,valor) == NULL){
+
+		list_add(segmentoLista,crearSegmento(nombreTabla,keyTabla,valor));
+	}
+	//Explicacion:
+	//se fija si existe la tabla, en ese caso se fija si ya hay alguien con esa key, si hay alguien lo reemplaza y sino agrega la pagina (not done yet)
+	//si no existe la tabla, la crea y la agrega
 }
+
+
+
 void mCreate(){
 
 }
@@ -134,6 +132,66 @@ void mJournal(){
 }
 void mGossip(){
 
+}
+
+//// Auxiliares
+
+t_config* read_config() {
+	return config_create("/home/utnso/tp-2019-1c-donSaturados/MemPool/Mem.config");
+}
+
+t_log* init_logger() {
+	return log_create("memPrincipal.log", "memPrincipal", 1, LOG_LEVEL_INFO);
+}
+
+segmento *crearSegmento(char* nombre,u_int16_t key,char* valor){
+	segmento *nuevo = malloc(sizeof(segmento));
+	nuevo->nombre = nombre;
+	nuevo->keyTabla = key;
+	nuevo->valor = valor;
+	nuevo->modificado = 0;
+	nuevo->timestamp = (unsigned)time(NULL);
+	return nuevo;
+}
+
+void handshakeConLissandra(u_int16_t lfsCliente,char* ipLissandra,u_int16_t puertoLissandra){
+	int conexionExitosa;
+	conexionExitosa = linkClient(&lfsCliente,ipLissandra , puertoLissandra);
+
+		if(conexionExitosa !=0){
+			perror("Error al conectarse con LFS");
+		}
+
+		recvData(lfsCliente, &tamanioMaximo, sizeof(u_int16_t));
+}
+
+
+void *buscarYreemplazar(u_int16_t keyTablaNueva,char* nuevoValor){
+
+	int tieneMismaKey(segmento *p){
+		if((p->keyTabla) == keyTablaNueva){
+				p->valor = nuevoValor;
+				p->timestamp = (unsigned)time(NULL);
+		}
+		return ((p->keyTabla) == keyTablaNueva);
+	}
+
+	return list_find(segmentoLista,(void*) tieneMismaKey);
+}
+
+void verMemoria(){
+	int index = 0;
+	segmento *aux = list_get(segmentoLista,index);
+
+		while(aux){
+			printf("Key: %i, Valor: %s TimeStamp: %d \n",aux->keyTabla,aux->valor,aux->timestamp);
+			index++;
+			aux = list_get(segmentoLista,index);
+		}//prueba para ver que hay en memoria nada mas
+
+		if(!aux){
+			printf("La memoria esta vacia o se termino de mostrar \n");
+		}
 }
 
 //list_destroy(segmentoLista);
--- MemPool/src/memPrincipal.h
+++ MemPool/src/memPrincipal.h
@@ -16,18 +16,25 @@
 
 typedef struct {
 	char* nombre;
+	char* valor;
 	u_int16_t keyTabla;
-}Segmento;
+	u_int16_t modificado;
+	long timestamp;
+}segmento;
 
 t_log* init_logger(void);
 t_config* read_config(void);
 
 void mSelect(char* nameTable,u_int16_t key);
-void mInsert(char*,u_int16_t,char*);
+void mInsert(char* nombreTabla,u_int16_t key,char* valor);
 void mCreate();
 void mDescribe();
 void mDrop();
 void mJournal();
 void mGossip();
+void handshakeConLissandra(u_int16_t,char*,u_int16_t);
+segmento *crearSegmento(char* nombre,u_int16_t key,char* value);
+void* buscarYreemplazar(u_int16_t ketTabla,char* valor);
+void verMemoria();
 
 #endif
